3dd1ea0f4e029cbb1cbb3424434289bac1db82d0
unknown <vaibhavnangia2001@gmail.com>
2022-09-02 13:18:40 +0530

    medium problems

---
+++ 11)avgInLevelfBT.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double>v;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            long temp=0;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                
+                if(node->left!=NULL){
+                    q.push(node->left);
+                }
+                if(node->right!=NULL){
+                    q.push(node->right);
+                }
+                temp+=node->val;
+            }
+            v.push_back((double)temp/size);
+        }return v;
+    }
+};
