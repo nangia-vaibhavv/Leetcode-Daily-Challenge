6fb9ca6224fcf6a3dd7901715972b932093d35ba
unknown <vaibhavnangia2001@gmail.com>
2022-09-07 17:30:35 +0530

    todays challenge

---
+++ 15)constructStringFromTree.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+         if(root==NULL)return NULL;
+        string ans=to_string(root->val);
+        if(root->left)
+        {
+            ans+='('+tree2str(root->left)+')';
+        }
+        if(root->right)
+        {
+            if(!root->left)ans+="()";
+             ans+='('+tree2str(root->right)+')';
+        }
+        return ans;
+    }
+};
