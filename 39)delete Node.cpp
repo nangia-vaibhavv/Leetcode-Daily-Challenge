1ea4a55adb1489943c4c8963af3567b286061c74
unknown <vaibhavnangia2001@gmail.com>
2022-10-13 08:36:09 +0530

    todays leetcode challenge

---
+++ 39)delete Node.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode *curr=node;
+        curr->val=curr->next->val;
+        curr->next=curr->next->next;
+        
+    }
+};
